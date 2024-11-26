d8e1f68b511b036769e3f8b0539bda306556835d
FlavioMatias <flaviomatiasnf@gmail.com>
2024-11-26 10:06:59 -0300

    Update same_network.py

--- Exercicios/same_network.py
+++ Exercicios/same_network.py
@@ -5,10 +5,15 @@ def same_network(ip1 : str, ip2 : str, mask : int) -> bool:
         raise ValueError("mascara invalida!")
     
     # separa os octetos e converte para binario
+    """ zfill usado para garantir que seja em 8 bits """
+    """ bin converte para binario """
+    """ '[2:]' para remover os 2 caracteres do binario """
+    """ join junta os octetos binarios com o parametro ''(nada) """
     bin_IP1 = ''.join(bin(int(octet))[2:].zfill(8) for octet in ip1.split('.'))
     bin_IP2 = ''.join(bin(int(octet))[2:].zfill(8) for octet in ip2.split('.'))
 
     # aplica a mascara nos IPs
+    """ fatia os binarios usando a mascara, preservando apenas os 24 bits a esquerda  """
     with_mask_IP1 = bin_IP1[:mask]
     with_mask_IP2 = bin_IP2[:mask]
 
